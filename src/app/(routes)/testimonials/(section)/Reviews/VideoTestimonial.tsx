42adb37ef98352f44749c327a2f6897dd5ab6a57
dhantosy <dhantosy@gmail.com>
2024-04-19 15:43:34 +0800

    feat: updated testimonials

--- src/app/(routes)/testimonials/(section)/Reviews/VideoTestimonial.tsx
+++ src/app/(routes)/testimonials/(section)/Reviews/VideoTestimonial.tsx
@@ -1,16 +1,68 @@
 'use client'
 
 import { VideoItem, VideoSection, Heading } from './styles'
+import { Autoplay, Pagination, Navigation } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { HiArrowLongRight, HiMiniXCircle } from "react-icons/hi2"
 
 export default function VideoTestimonial() {
   return (
     <>
       <Heading>Interviews with the Students</Heading>
       <VideoSection>
-        <div>
-          <VideoItem>
-            <iframe src="https://www.youtube.com/embed/_oEsunZAzRg" title="Empowered by Knowledge: My Joga Yoga Teacher Training Review" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope"></iframe>
-          </VideoItem>
+        <Swiper
+          loop
+          navigation
+          modules={[Autoplay, Navigation]}
+          pagination={{ clickable: true }}
+          breakpoints={{
+            1: {
+              slidesPerView: 1,
+              spaceBetween: 0,
+            },
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 0,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 0,
+            },
+          }}
+          slidesPerView={4}
+        >
+          <SwiperSlide>
+            <VideoItem>
+              <iframe src="https://www.youtube.com/embed/shWPub9onxY" title="Empowered by Knowledge: My Joga Yoga Teacher Training Review" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope"></iframe>
+            </VideoItem>
+          </SwiperSlide>
+          <SwiperSlide>
+            <VideoItem>
+              <iframe src="https://www.youtube.com/embed/mC5fEZhWgmE" title="Empowered by Knowledge: My Joga Yoga Teacher Training Review" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope"></iframe>
+            </VideoItem>
+          </SwiperSlide>
+          <SwiperSlide>
+            <VideoItem>
+              <iframe src="https://www.youtube.com/embed/g5TnhzXnWig" title="Empowered by Knowledge: My Joga Yoga Teacher Training Review" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope"></iframe>
+            </VideoItem>
+          </SwiperSlide>
+          <SwiperSlide>
+            <VideoItem>
+              <iframe src="https://www.youtube.com/embed/xVW0hEQw6q4" title="Empowered by Knowledge: My Joga Yoga Teacher Training Review" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope"></iframe>
+            </VideoItem>
+          </SwiperSlide>
+          <SwiperSlide>
+            <VideoItem>
+              <iframe src="https://www.youtube.com/embed/_Hp0SQpxgYg" title="Empowered by Knowledge: My Joga Yoga Teacher Training Review" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope"></iframe>
+            </VideoItem>
+          </SwiperSlide>
+          <SwiperSlide>
+            <VideoItem>
+              <iframe src="https://www.youtube.com/embed/_oEsunZAzRg" title="Empowered by Knowledge: My Joga Yoga Teacher Training Review" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope"></iframe>
+            </VideoItem>
+          </SwiperSlide>
+        </Swiper>
+        {/* <div>
           <VideoItem>
             <div />
             <iframe src="https://www.youtube.com/embed/_Hp0SQpxgYg" title="Unveiling My Yoga Teacher Training Experience" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
@@ -23,7 +75,7 @@ export default function VideoTestimonial() {
             <div />
             <iframe src="https://www.youtube.com/embed/g5TnhzXnWig" title="Ratna Shows Her True Experience On The 200 Hour Joga Yoga Teacher Training In Bali" frameBorder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
           </VideoItem>
-        </div>
+        </div> */}
       </VideoSection>
     </>
   )
