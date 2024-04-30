d2fb3ca345d0d41102c09f5912df5504a99e43c1
dhantosy <dhantosy@gmail.com>
2024-04-30 15:18:41 +0800

    feat: load yt only when clicked

--- src/app/_components/NumbersSection/index.tsx
+++ src/app/_components/NumbersSection/index.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useState } from 'react';
+import Image from 'next/image';
 import dynamic from 'next/dynamic';
 const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
 import Container from '@/app/_components/Container'
@@ -7,8 +9,12 @@ import { PiCertificateBold } from "react-icons/pi";
 import { GrYoga } from 'react-icons/gr';
 import { FaStar } from 'react-icons/fa6';
 import { Section, Title, Heading, Content, ContentHeading, ContentItem, Icon, VideoWrapper, VideoContent } from './styles'
+import ImageVideoOne from 'public/assets/images/video-image-1.png'
+import ImageVideoTwo from 'public/assets/images/video-image-2.png'
 
 export default function NumbersSection() {
+  const [videoOneLoad, setVideoOneLoad] = useState(false)
+  const [videoTwoLoad, setVideoTwoLoad] = useState(false)
 
   return (
     <Section>
@@ -54,22 +60,66 @@ export default function NumbersSection() {
         <VideoContent>
           <VideoWrapper>
             <div className='player-wrapper'>
-              <ReactPlayer
-                url='https://www.youtube.com/watch?v=EyMeJcJyGKk'
-                className='react-player'
-                width='100%'
-                height='100%'
-              />
+              {!videoOneLoad ? (
+                <div onClick={() => setVideoOneLoad(true)}>
+                  <Image
+                    src={ImageVideoOne.src}
+                    placeholder='blur'
+                    blurDataURL={ImageVideoOne.blurDataURL}
+                    alt='YTT'
+                    sizes={`100vw, 33vw`}
+                    fill
+                    style={{ objectFit: 'cover' }}
+                    className='image'
+                  />
+                </div>
+              ) : (
+                <ReactPlayer
+                  url='https://www.youtube.com/watch?v=EyMeJcJyGKk'
+                  className='react-player'
+                  width='100%'
+                  height='100%'
+                  config={{
+                    youtube: {
+                      playerVars: {
+                        autoplay: 1
+                      }
+                    }
+                  }}
+                />
+              )}
             </div>
           </VideoWrapper>
           <VideoWrapper>
             <div className='player-wrapper'>
-              <ReactPlayer
-                url='https://www.youtube.com/watch?v=IOnxrTPBktY'
-                className='react-player'
-                width='100%'
-                height='100%'
-              />
+              {!videoTwoLoad ? (
+                <div onClick={() => setVideoTwoLoad(true)}>
+                  <Image
+                    src={ImageVideoTwo.src}
+                    placeholder='blur'
+                    blurDataURL={ImageVideoTwo.blurDataURL}
+                    alt='YTT'
+                    sizes={`100vw, 33vw`}
+                    fill
+                    style={{ objectFit: 'cover' }}
+                    className='image'
+                  />
+                </div>
+              ) : (
+                <ReactPlayer
+                  url='https://www.youtube.com/watch?v=IOnxrTPBktY'
+                  className='react-player'
+                  width='100%'
+                  height='100%'
+                    config={{
+                      youtube: {
+                        playerVars: {
+                          autoplay: 1
+                        }
+                      }
+                    }}
+                />
+              )}
             </div>
           </VideoWrapper>
         </VideoContent>
